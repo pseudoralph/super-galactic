bbd7f73d452fbc9edd6278963a085ab2c0bab8f1
Ralph Perdomo <ralph@prdmo.com>
2019-01-11 11:34:53 -0800

    add ui element for life expectancy

--- src/draw-ui.js
+++ src/draw-ui.js
@@ -5,17 +5,41 @@ export function ui() {
 
   document.addEventListener('readystatechange', event => {
     if (event.target.readyState === 'interactive') {
+      let entry = document.getElementById('app');
+      let payload = document.createElement('div');
+      payload.setAttribute('class','card');
 
+      let rawHtml = `
+  <h5 class="card-header">Life expectancy</h5>
+    <div class="card-body">
+      <div class="form-group">
+        <label for="country">Country:</label>
+        <select class="form-control" id="country">`;
+      
+      for (let c = 0; c<countries.length; c++) {
+        rawHtml+=`
+          <option>${countries[c]}</option>`; 
+      }
 
-     
+      rawHtml+=`
+        </select>
+      </div>
+      <div class="form-group">
+        <div class="custom-control custom-radio custom-control-inline">
+          <input type="radio" id="female" name="gender" class="custom-control-input">
+          <label class="custom-control-label" for="female">Female</label>
+        </div>
+        <div class="custom-control custom-radio custom-control-inline">
+          <input type="radio" id="male" name="gender" class="custom-control-input">
+          <label class="custom-control-label" for="male">Male</label>
+        </div>
+      </div>
+    </div>`;
 
-      console.log(countries);
-
-
+      payload.innerHTML=rawHtml;
+      entry.appendChild(payload);
     }
 
   });
-
-
-
 }
+
